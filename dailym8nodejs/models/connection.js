88d0defe2cc8d788a4414408758c6ecd99f83769
João Carvalho <joaove>
2022-11-22 14:17:13 +0000

    commit

--- dailym8nodejs/models/connection.js
+++ dailym8nodejs/models/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg'); 
  
-const connectionString = "postgres://postgres:tftprojeto@localhost:5432/dailym8nodejs"  //Conectar com os dados normais da BD postgresql
+const connectionString = "postgres://postgres:rFeZvIpl3lmOXfB@dailym8nodejs.internal:5432"  //Conectar com os dados normais da BD postgresql
 const Pool = pg.Pool 
 /*const pool = new Pool({ 
     connectionString, 
@@ -12,7 +12,7 @@ const Pool = pg.Pool
 }) */
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:tftprojeto@localhost:5432/dailym8nodejs',
+    connectionString: process.env.DATABASE_URL || 'postgres://postgres:rFeZvIpl3lmOXfB@dailym8nodejs.internal:5432',
     ssl: process.env.DATABASE_URL ? true : false
 })
  
